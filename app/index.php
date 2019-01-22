e33caaebfe58e6e5ea638062fc1712637fe03abb
FelippeChemello <felippechemello850@outlook.com>
2019-01-22 16:21:09 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -3,6 +3,19 @@
 
   echo "Teste KingHost";
 
+  $servername = "192.168.0.33:3306";
+  $database = "teste";
+  $username = "teste";
+  $password = "1234567";
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $database);
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+  mysqli_close($conn);
+
 
 ?>
 
