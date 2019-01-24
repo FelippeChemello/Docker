5b4ab2e98bbc0d6fd7b3628a9bb410614b383d63
FelippeChemello <felippechemello850@outlook.com>
2019-01-24 08:27:30 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,20 +1,6 @@
 <?php
 
-
   echo "Teste KingHost";
-
-  $banco = "teste";
-  $usuario = "teste";
-  $senha = "1234567";
-  $hostname = "localhost:3306";
-  $conn = mysqli_connect($hostname,$usuario,$senha); mysqli_select_db($banco) or die( "Não foi possível conectar ao banco MySQL");
-  if (!$conn) {echo "Não foi possível conectar ao banco MySQL.
-  "; exit;}
-  else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
-  ";}
-  mysql_close(); 
-  
-
 
 ?>
 
