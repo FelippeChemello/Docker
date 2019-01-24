0715c6f3272c5c8d8ffb8cfe8f3ae252f851bc63
FelippeChemello <felippechemello850@outlook.com>
2019-01-24 12:12:58 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -2,19 +2,5 @@
 
   echo "Teste KingHost";
 
-$link = mysqli_connect("mysql", "teste", "1234567", "teste");
- 
-if (!$link) {
-    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
- 
-echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
- 
-mysqli_close($link);
-
-
 ?>
 
